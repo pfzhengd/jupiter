eb441e204d31444a7b23a20abbdbed4bb9c0eb54
Zekai <zekai@bit365.org>
2026-02-25 00:10:20 +0800

    chore: 整理目录

--- packages/utils/test/LocalStorageStore.spec.ts
+++ packages/utils/test/LocalStorageStore.spec.ts
@@ -1,8 +1,8 @@
-import { LocalDataBase } from '../src'
+import { LocalStorageStore } from '../src'
 
 test('1. 测试 LocalDataBase 实例', (done) => {
-  const localDataBase = new LocalDataBase({ key: 'test', initData: () => ({}) })
-  expect(localDataBase).toBeInstanceOf(LocalDataBase)
+  const localDataBase = new LocalStorageStore({ key: 'test', initData: () => ({}) })
+  expect(localDataBase).toBeInstanceOf(LocalStorageStore)
   expect(localDataBase.data).toEqual({})
   expect(localDataBase.key).toBe('test')
   expect(localDataBase.options).toEqual({ key: 'test', initData: expect.any(Function) })
@@ -10,7 +10,7 @@ test('1. 测试 LocalDataBase 实例', (done) => {
 })
 
 test('2. 测试 LocalDataBase 实例的 saveup 方法', (done) => {
-  const localDataBase = new LocalDataBase({ key: 'test', initData: () => ({}) })
+  const localDataBase = new LocalStorageStore({ key: 'test', initData: () => ({}) })
   localDataBase.saveup()
   expect(localStorage.getItem('test')).toBe('{}')
   expect(localDataBase.data).toEqual({})
